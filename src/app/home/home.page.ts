a85dc2ece1292b03f2e2ab8f42dd46e329aa2595
Vitor <vitor.anjos.santoss@gmail.com>
2019-08-29 20:52:39 -0300

    Botão de Sair

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Storage } from '@ionic/storage';
 
+import { NavController } from '@ionic/angular';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -11,13 +13,18 @@ export class HomePage implements OnInit {
 
   usuario: any = "";
 
-  constructor(private storage: Storage) { }
+  constructor(private storage: Storage,private navctrl: NavController) { }
 
   ngOnInit() {
     this.storage.get("usuario").then((dados) => {
       this.usuario = dados;
       //console.log(dados);
     });
+
   }
 
+  sair(){
+    this.storage.clear();
+    this.navctrl.navigateRoot("/");
+  }
 }
