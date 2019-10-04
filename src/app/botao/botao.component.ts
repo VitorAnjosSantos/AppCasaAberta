5e396e6719d024e39dbc7326d83cebf038736999
Vitor <vitor.anjos.santoss@gmail.com>
2019-10-03 22:16:29 -0300

    Criação de component

---
+++ src/app/botao/botao.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-botao',
+  templateUrl: './botao.component.html',
+  styleUrls: ['./botao.component.scss'],
+})
+export class BotaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  botao(){
+    alert("Click");
+  }
+
+}
